7d082248ed456782fc5ff8ab5160108b593c6523
jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-03-14 13:54:10 -0400

    done

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {DefaultComponent} from './default.component';
+import {DashboardComponent} from '../../modules/dashboard/dashboard.component';
+import {RouterModule} from '@angular/router';
+import {PostsComponent} from '../../modules/posts/posts.component';
+import {SharedModule} from '../../shared/shared.module';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatCardModule} from '@angular/material/card';
+import {DashboardService} from '../../modules/dashboard.service';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableModule} from '@angular/material/table';
+
+
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    PostsComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    MatDividerModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatTableModule
+  ],
+  providers: [
+    DashboardService
+  ]
+})
+export class DefaultModule { }
